a23334be19b3955ae0363840d564b28d4565c907
Edmund Hans Tapado <69574544+monmontapado@users.noreply.github.com>
2022-10-11 00:45:44 +0800

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,19 @@ const port = 3000
 
 var webserver = require('./routes/webserver');
 
+const { spawn } = require('child_process');
+
+const childPython = spawn('python', ['slp_sourcecode.py']);
+
+childPython.stderr.on('data', (data) => {
+    console.error('There was an error: ${data}');
+});
+
+childPython.on('close', (code) => {
+    console.log('child process exited with code ${code}');
+});
+
+
 // another way of opening html file in nodejs
 // app.use(express.static(__dirname+'/frontend'));
 // app.get("/", function(req,res){
