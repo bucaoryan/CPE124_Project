c007b2bd057f8b2405a2d7c2ab2d9d8fd11337fe
Ryan Bucao <69749867+bucaoryan@users.noreply.github.com>
2022-10-12 18:54:03 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,63 +3,50 @@ const express = require("express");
 var app = express()
 const path = require("path")
 const port = 3000
-var socket = require("socket.io");
+var Gpio = require('onoff').Gpio;
+const pir = new Gpio(20, "in", "both"); //
+const {spawn} = require('child_process')
 var webserver = require('./routes/webserver');
-//var Gpio = require('onoff').Gpio;
-//const pir = new Gpio(27, "in", "both"); //pin 27 pir
 
-// another way of opening html file in nodejs
-// app.use(express.static(__dirname+'/frontend'));
-// app.get("/", function(req,res){
-//   res.sendFile(
-//     path.join(__dirname,"/frontend/web.html")
-//   );
-// });
 
-//initialize server
-const server = app.listen(port, function () {
-    console.info("Web App: http://localhost:%s", port);
-});
-
-//routes
+//routes    
 app.use(express.static(path.join(__dirname, 'frontend')));
 app.use("/", webserver);
 
 //call python script (slp_sourcecode.py) for the system
-const { spawn } = require('child_process');
-
-const childPython = spawn('python', ['slp_sourcecode.py']);
-
-childPython.stderr.on('data', (data) => {
-    console.error('There was an error: ${data}');
-});
-
-childPython.on('close', (code) => {
-    console.log('child process exited with code ${code}');
-});
-
-// initiale socket.io 
-const io = socket(server); 
-var totalCount = 50;
-var numberOfPumps = 15;
-var measurement_ml = 450;
-
-io.on("connection",(socket) => {
-    console.log('new connection:'+socket.id);
-    // pir.watch(function(err, value) {
-    //     if (err) exit();
-    //     if (value == 1){
-    //         console.log('SYSTEM ACTIVATED')
-    //         //totalCount++;
-    //     } else {
-    //         console.log('NO MOTION DETECTED')
-    //     }
-        // console.log(value ? "SYSTEM ACTIVATED." : "NO MOTION DETECTED.");    
-        // totalCount++; 
-        // console.log(totalCount) 
-    //});     
-    io.emit("message", totalCount)
-    socket.on("disconnect", () =>{
-        console.log('disconnected');
-    })
+function runScript(){
+    return spawn('python', [
+        "-u",
+        path.join(__dirname, 'slp_sourcecode.py')
+    ]);
+}
+pir.watch((err,value)=>{
+    if (err) exit();
+    console.log('Pumping Alcohol')
+    runScript()
 })
+function exit(err) {
+  if (err) console.log("An error occurred: " + err);
+  pir.unexport();
+  console.log("Bye, bye!");
+  process.exit();
+}
+process.on("SIGINT", exit);
+
+//print output of script
+const subprocess = runScript()
+subprocess.stdout.on('data', (data) => {
+  console.log(`data:${data}`);
+});
+subprocess.stderr.on('data', (data) => {
+  console.log(`error:${data}`);
+});
+subprocess.on('close', () => {
+  console.log("Closed");
+});
+
+app.listen(port, function () {
+    console.info("Web App: http://localhost:%s", port);
+});
+
+
