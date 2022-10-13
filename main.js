51b60f66240da4f39488305bbec583bd76a1a602
Ryan Bucao <69749867+bucaoryan@users.noreply.github.com>
2022-10-13 22:08:55 +0800

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ const pir = new Gpio(20, "in", "both"); //
 const {spawn} = require('child_process')
 var webserver = require('./routes/webserver');
 
-
 //routes    
 app.use(express.static(path.join(__dirname, 'frontend')));
 app.use("/", webserver);
@@ -20,27 +19,33 @@ function runScript(){
         path.join(__dirname, 'slp_sourcecode.py')
     ]);
 }
+
 pir.watch((err,value)=>{
     if (err) exit();
     console.log('Pumping Alcohol')
     runScript()
 })
+
 function exit(err) {
   if (err) console.log("An error occurred: " + err);
   pir.unexport();
   console.log("Bye, bye!");
   process.exit();
 }
+
 process.on("SIGINT", exit);
 
 //print output of script
 const subprocess = runScript()
+
 subprocess.stdout.on('data', (data) => {
   console.log(`data:${data}`);
 });
+
 subprocess.stderr.on('data', (data) => {
   console.log(`error:${data}`);
 });
+
 subprocess.on('close', () => {
   console.log("Closed");
 });
@@ -49,4 +54,12 @@ app.listen(port, function () {
     console.info("Web App: http://localhost:%s", port);
 });
 
+const ipAdd="ENTER YOUR IP HERE";
 
+app.listen(port, ipAdd || "localhost" ,() => {
+    console.log(`For Mobile Phone: http://${ipAdd}:${port}`);
+});
+
+
+
+  
